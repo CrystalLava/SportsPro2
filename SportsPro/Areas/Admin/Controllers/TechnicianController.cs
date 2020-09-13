810b415748e78c558094591002b38658af4c1d3f
Hui Jillain <Hui.Jillain@edu.sait.ca>
2020-09-12 20:56:50 -0600

    add views in Areas/Admin

---
+++ SportsPro/Areas/Admin/Controllers/TechnicianController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SportsPro.Areas.Admin.Controllers
+{
+    public class TechnicianController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
