1db4e0b74e37930e7426593ba7db4c18c54635fd
huijillain <skyjillain@gmail.com>
2020-09-30 17:02:38 -0600

    Added Registration Views, RegistrationViewModel in ViewModels

---
+++ SportsPro/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace SportsPro.Models
+{
+    public class RegisterViewModel
+    {
+
+        private List<Customer> customers;
+        public List<Customer> Customers
+        {
+            get => customers;
+
+            set
+            {
+                customers = value;
+            }
+
+        }
+        public string FilterString { get; set; }
+        public string ActiveIncident { get; set; }
+        public string ActiveTechnician { get; set; }
+        public string ActiveCustomer { get; set; }
+        public string Action { get; set; }
+
+        public Incident CurrentIncident { get; set; }
+
+        public Customer CurrentCustomer { get; set; }
+
+        public Product CurrentProduct { get; set; }
+
+        public int? CustomerID { get; set; }
+
+        private List<Product> products;
+
+        public List<Product> Products
+        {
+            get => products;
+
+            set
+            {
+                products = value;
+            }
+
+        }
+
+        private List<Incident> incidents;
+
+        public List<Incident> Incidents
+        {
+            get => incidents;
+
+            set
+            {
+                incidents = value;
+            }
+
+        }
+
+        public int? TechnicianID { get; set; }
+
+        private List<Technician> technicians;
+        public List<Technician> Technicians
+        {
+            get => technicians;
+
+            set
+            {
+                technicians = value;
+            }
+
+        }
+        public string CheckActiveIncident(string i) =>
+            i.ToLower() == ActiveIncident.ToLower() ? "active" : "";
+        public string CheckActiveTechnician(string t) =>
+            t.ToLower() == ActiveTechnician.ToLower() ? "active" : "";
+
+    }
+}
