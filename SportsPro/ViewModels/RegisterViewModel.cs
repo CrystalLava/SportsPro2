c4f767e6e3bed111200cea54665b9da6e842fc45
huijillain <skyjillain@gmail.com>
2020-10-01 11:42:13 -0600

    Make sure Registration pages work

--- SportsPro/ViewModels/RegisterViewModel.cs
+++ SportsPro/ViewModels/RegisterViewModel.cs
@@ -10,17 +10,14 @@ namespace SportsPro.Models
 {
     public class RegisterViewModel
     {
-
         private List<Customer> customers;
         public List<Customer> Customers
         {
             get => customers;
-
             set
             {
                 customers = value;
             }
-
         }
         public string FilterString { get; set; }
         public string ActiveIncident { get; set; }
@@ -41,12 +38,10 @@ namespace SportsPro.Models
         public List<Product> Products
         {
             get => products;
-
             set
             {
                 products = value;
             }
-
         }
 
         private List<Incident> incidents;
@@ -54,12 +49,10 @@ namespace SportsPro.Models
         public List<Incident> Incidents
         {
             get => incidents;
-
             set
             {
                 incidents = value;
             }
-
         }
 
         public int? TechnicianID { get; set; }
@@ -68,12 +61,10 @@ namespace SportsPro.Models
         public List<Technician> Technicians
         {
             get => technicians;
-
             set
             {
                 technicians = value;
             }
-
         }
         public string CheckActiveIncident(string i) =>
             i.ToLower() == ActiveIncident.ToLower() ? "active" : "";
