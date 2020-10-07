2ad7cae819a4e7ac389b8092badd840972b953b4
huijillain <skyjillain@gmail.com>
2020-10-07 09:44:23 -0600

    install Microsoft.AspNetCore.Identity.EntityFrameworkCore;

--- SportsPro/Models/Customer.cs
+++ SportsPro/Models/Customer.cs
@@ -29,8 +29,8 @@ namespace SportsPro.Models
 		public string CountryID { get; set; }
 		public Country Country { get; set; }
 
-		////[Required(ErrorMessage = "Phone number must be in (999)-999-9999 format.")]
-		[RegularExpression(@"^((\+0?1\s)?)\(?\d{3}\)?[\s.\s]\d{3}[\s.-]\d{4}$")]
+		[RegularExpression(@"^((\+0?1\s)?)\(?\d{3}\)?[\s.\s]\d{3}[\s.-]\d{4}$",
+			ErrorMessage = "Phone number must be in the (999) 999-9999 format.")]
 		public string Phone { get; set; }
 
 		[Required(ErrorMessage = "Please enter a valid email address.")]
